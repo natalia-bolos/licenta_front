b9b618259256de89eed10eee1f226251d65b81dc
natalia-bolos <natalia_bolos@yahoo.com>
2020-08-12 14:50:55 +0300

    add sign in and sign up functionality

--- src/user/login.jsx
+++ src/user/login.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import loginImg from "../login.svg";
+import { ACCESS_TOKEN, USER_ID } from '../constants';
+import { login } from '../util/ApiUtils';
 
 export class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+      username:"",
+      password:""
+    };
+    this.handleUsernameInputChange = this.handleUsernameInputChange.bind(this);
+    this.handlePasswordInputChange=this.handlePasswordInputChange.bind(this);
+    this.loginApiCallOnClick=this.loginApiCallOnClick.bind(this);
   }
 
+  handleUsernameInputChange(event) {
+    this.setState({username: event.target.value})
+    
+  }
+  handlePasswordInputChange(event) {
+    this.setState({password: event.target.value})
+  }
+
+  loginApiCallOnClick(){
+    login({"usernameOrEmail":this.state.username,"password":this.state.password}).then(response => {
+      localStorage.setItem(ACCESS_TOKEN, response.accessToken);
+      localStorage.setItem(USER_ID,response.userId);
+      console.log("logged in ok!");
+      console.log(localStorage.getItem(ACCESS_TOKEN));
+      console.log(localStorage.getItem(USER_ID));
+  })
+}
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
@@ -17,16 +43,18 @@ export class Login extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username:</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username"  value={this.state.username} 
+                       onChange={this.handleUsernameInputChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password:</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password" value={this.state.password} 
+                       onChange={this.handlePasswordInputChange}/>
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={this.loginApiCallOnClick}>
             Login
           </button>
         </div>
--- src/user/register.jsx
+++ src/user/register.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import loginImg from "../register.svg";
-
+import { signup } from '../util/ApiUtils';
 export class Register extends React.Component {
   constructor(props) {
     super(props);
+    this.state={
+      name:"",
+      username:"",
+      email:"",
+      password:""
+    };
+    this.handleUsernameInputChange = this.handleUsernameInputChange.bind(this);
+    this.handlePasswordInputChange=this.handlePasswordInputChange.bind(this);
+    this.handleNameInputChange=this.handleNameInputChange.bind(this);
+    this.handleEmailInputChange=this.handleEmailInputChange.bind(this);
+    this.signUpApiCallOnClick=this.signUpApiCallOnClick.bind(this);
+  }
+  handleUsernameInputChange(event) {
+    this.setState({username: event.target.value})
+    
+  }
+  handlePasswordInputChange(event) {
+    this.setState({password: event.target.value})
+  }
+  handleNameInputChange(event){
+    this.setState({name: event.target.value})
+  }
+  handleEmailInputChange(event){
+    this.setState({email: event.target.value})
   }
 
+  signUpApiCallOnClick(){
+      signup({
+        "name":this.state.name,
+        "username":this.state.username,
+        "mail":this.state.email,
+        "password":this.state.password,
+      }).then(response => {
+        // localStorage.setItem(ACCESS_TOKEN, response.accessToken);
+        console.log("registered ok!");
+        // console.log(localStorage.getItem(ACCESS_TOKEN));
+    })
+  }
+
+  
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
@@ -17,24 +55,28 @@ export class Register extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="name">Name:</label>
-              <input type="text" name="name" placeholder="name" />
+              <input type="text" name="name" placeholder="name" value={this.state.name} 
+                       onChange={this.handleNameInputChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="username">Username:</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username" value={this.state.username} 
+                       onChange={this.handleUsernameInputChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="email">Email:</label>
-              <input type="text" name="email" placeholder="email" />
+              <input type="text" name="email" placeholder="email" value={this.state.email} 
+                       onChange={this.handleEmailInputChange}/>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password:</label>
-              <input type="text" name="password" placeholder="password" />
+              <input type="text" name="password" placeholder="password" value={this.state.password} 
+                       onChange={this.handlePasswordInputChange} />
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={this.signUpApiCallOnClick}>
             Register
           </button>
         </div>
