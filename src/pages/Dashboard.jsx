948891b49463450570be08e20eb32dcc4ab12984
natalia-bolos <natalia_bolos@yahoo.com>
2020-08-25 15:12:41 +0300

    adapt the adition of a response to a list

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { GroupList } from "../groups/GroupList";
-import { USER_NAME,USER_ID } from '../constants';
-import { getPostsOfGroup, getMembersOfGroup,getGroupsOfUser,addPostToGroup,createGroup } from '../util/ApiUtils';
-import {GroupData} from "../groups/GroupData";
-import {GroupMembers} from "../groups/GroupMembers";
+import { USER_NAME, USER_ID } from '../constants';
+import { getPostsOfGroup, getMembersOfGroup, getGroupsOfUser, addPostToGroup, createGroup } from '../util/ApiUtils';
+import { GroupData } from "../groups/GroupData";
+import { GroupMembers } from "../groups/GroupMembers";
 import Textimput from '../groups/Textimput';
 import CreateGroup from '../groups/CreateGroup';
 export default class Dashboard extends React.Component {
@@ -12,57 +12,60 @@ export default class Dashboard extends React.Component {
         this.state = {
             selectedGroupName: "",
             selectedGroupId: 0,
-            groups:[],
-            posts:[],
-            members:[]
-          };
+            groups: [],
+            posts: [],
+            members: []
+        };
 
     }
 
     componentDidMount() {
-        getGroupsOfUser(localStorage.getItem(USER_ID)).then(response=>{
-            this.setState({groups:response})
+        getGroupsOfUser(localStorage.getItem(USER_ID)).then(response => {
+            this.setState({ groups: response })
             if (response !== undefined && response.length != 0) {
-                this.setSelectedGroup(response[0].groupId,response[0].name);
+                this.setSelectedGroup(response[0].groupId, response[0].name);
             }
-            
-            
+
+
         })
     }
 
-    setSelectedGroup(selectedGroup,selectedGroupName){
+    setSelectedGroup(selectedGroup, selectedGroupName) {
         Promise.all([
             getPostsOfGroup(selectedGroup),
             getMembersOfGroup(selectedGroup)
-        ]).then(([retreivedPosts,retreivedMembers])=>{
-            this.setState({selectedGroupName:selectedGroupName,posts:retreivedPosts,members:retreivedMembers,selectedGroupId:selectedGroup});
-        })  
+        ]).then(([retreivedPosts, retreivedMembers]) => {
+            this.setState({ selectedGroupName: selectedGroupName, posts: retreivedPosts, members: retreivedMembers, selectedGroupId: selectedGroup });
+        })
     }
 
-    createNewGroup(group){
-            createGroup(group).then(response=>{
-                var existingGroups=this.state.groups;
-                existingGroups.push({
-                    response
-                })
-                this.setState({groups:existingGroups});
-                console.log("created");
+    createNewGroup(group) {
+        createGroup(group).then(response => {
+            var existingGroups = this.state.groups;
+            existingGroups.push({
+                groupId: response.groupId,
+                name: response.name,
+                type: response.type,
+                description: response.description
             })
+            this.setState({ groups: existingGroups });
+            console.log("created");
+        })
     }
 
     createNewPost(text) {
-        var postToAdd={groupId:this.state.selectedGroupId, userId:localStorage.getItem(USER_ID),text:text}
+        var postToAdd = { groupId: this.state.selectedGroupId, userId: localStorage.getItem(USER_ID), text: text }
         addPostToGroup(postToAdd).then(response => {
-            var posted=this.state.posts;
+            var posted = this.state.posts;
             posted.push({
-                postId:response.groupPostId,
-                userId:response.userId,
-                username:localStorage.getItem(USER_NAME),
-                text:response.text,
-                timestamp:response.timestamp,
-                comments:[]
+                postId: response.groupPostId,
+                userId: response.userId,
+                username: localStorage.getItem(USER_NAME),
+                text: response.text,
+                timestamp: response.timestamp,
+                comments: []
             })
-            this.setState({posts:posted});
+            this.setState({ posts: posted });
         })
     }
 
@@ -72,16 +75,16 @@ export default class Dashboard extends React.Component {
                 <div className="row">
                     <h4>Hello, {localStorage.getItem(USER_NAME)}</h4>
                     <div className="col s2 ">
-                        <GroupList   groups={this.state.groups} onGroupClicked={this.setSelectedGroup.bind(this)}/>
+                        <GroupList groups={this.state.groups} onGroupClicked={this.setSelectedGroup.bind(this)} />
                         <CreateGroup createNewGroup={this.createNewGroup.bind(this)} />
                     </div>
-                   
+
                     <div className="col s8">
-                        <GroupData groupName={this.state.selectedGroupName} posts={this.state.posts}/>
-                        <Textimput createNewPost={this.createNewPost.bind(this)}/>
+                        <GroupData groupName={this.state.selectedGroupName} posts={this.state.posts} />
+                        <Textimput createNewPost={this.createNewPost.bind(this)} />
                     </div>
                     <div className="col s2">
-                        <GroupMembers members={this.state.members}/>
+                        <GroupMembers members={this.state.members} />
                     </div>
                 </div>
             </div>)
